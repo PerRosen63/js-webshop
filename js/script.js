477eecb9a749d1b1fca26365fd66c01409ceeff8
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2023-11-21 01:08:57 +0100

    börjat med psuedokod

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//PSUEDOKOD
+/**
+ * 1. Skriv statisk HTML samt grundläggande CSS
+ * 
+ * 2. PRODUKTSEKTION
+ * 
+ * - gör array med objekt för varje produkt
+ * 
+ * - printa ut produkter inkl. plus- och minusknappar för varje produkt
+ * 
+ * - eventslyssnare på plus- och minusknappar
+ * 
+ * 3. VARUKORG
+ * 
+ * - printa ut alla produkter med amount > 0
+ * 
+ * - summa
+ * 
+ * 4.FORMULÄR
+ * 
+ * - html
+ * 
+ * - conditional radiobuttons Faktura eller Kort
+ * 
+ * - validering
+ * 
+ * - 
+ * 
+ * 
+ * 
+ * 
+ */
