c9c2b95e04efd47cb6ef429d0978aa889713740a
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2023-11-22 17:14:40 +0100

    Uppdaterat psuedokod

--- js/script.js
+++ js/script.js
@@ -10,11 +10,19 @@
  * 
  * - eventslyssnare på plus- och minusknappar
  * 
+ * - "Osynligt" helgpåslag 15%
+ * 
+ * - Mängdrabatt/artikel, minst 10st 10%
+ * 
  * 3. VARUKORG
  * 
  * - printa ut alla produkter med amount > 0
  * 
  * - summa
+ * 
+ * - Måndagsrabatt 03:00-10.00: 10 % på hela beställningen (synligt)
+ * 
+ * - Minst 15 varor = gratis frakt. 25kr + 10% av tot.bel.
  * 
  * 4.FORMULÄR
  * 
@@ -24,7 +32,25 @@
  * 
  * - validering
  * 
- * - 
+ * - event på Beställ-knapp som öppnar Bekräftelse-ruta
+ * 
+ * - Faktura högst 800kr
+ * 
+ * - Utgråad beställ-knapp
+ * 
+ * - Timeout efter 15 min med meddelande att beställaren är för långsam samt rensning av formulär. Start vid laddning av sida, klick i produktdelen eller klick i formulär. 
+ * 
+ * 5. SORTERA
+ * 
+ * - selectbox? radiobuttons?
+ * 
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  * 
  * 
  * 
