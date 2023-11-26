9d6a3d1296c9d49eba10b5a8cd30770d32b440e3
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2023-11-27 00:57:01 +0100

    js produkter och varukorg, plus/minus-knappar etc

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const products = [
     {
         name: 'Affisch',
@@ -121,23 +123,50 @@ const products = [
     }
 ];
 
-//Amount
-//const amount = 0;
-
 
 const prodContainer = document.querySelector('#productContainer');
-//console.log(prodContainer);
+const cartContainer = document.querySelector('#cartContainer');
 
-for (let i = 0; i < products.length; i++) {
+function decreaseAmount(e) {
+    const index = e.currentTarget.dataset.id
+    if (products[index].amount > 0) {
+    products[index].amount -= 1;
+    printProducts();
+    printCartProducts();
+    }
+};
+
+
+function increaseAmount(e) {
+    const index = e.currentTarget.dataset.id
+    products[index].amount += 1;
+    printProducts();
+    //printCartProducts();
+};  
+
+function resetAmount(e) {
+    const index = e.currentTarget.dataset.id
+    products[index].amount = 0;
+    //printProducts();
+    printCartProducts();
+    console.log(index);
+}; 
+    
+
+
+function printProducts() {
+    prodContainer.innerHTML = ''; //töm dokument
+
+    for (let i = 0; i < products.length; i++) {
     const product = products[i];
+
     const amount = product.amount;
     const sum = amount * product.price;
+
     //Rating
-    const productRating = `${product.rating}`;
+    const productRating = product.rating;
     const productRatingSuffix = productRating * 10;
-    console.log(sum);
-
-
+    
     prodContainer.innerHTML += `
         <article>
         <img class="productImage" src="${product.image.src}" alt="${product.image.alt}" width="300" height="300" loading="lazy" >
@@ -145,10 +174,11 @@ for (let i = 0; i < products.length; i++) {
         <p class="productCategory"><span>Kategori: </span>${product.category}</p>
         <h4 class=productPrice><span>Pris: </span>${product.price}:-</h4>
         <div>
-            <span class="material-symbols-outlined">indeterminate_check_box</span>            
-            <span class="material-symbols-outlined">add_box</span>
+            <button data-id="${i}" class="minus material-symbols-outlined">indeterminate_check_box</button>            
+            <button data-id="${i}" class="plus material-symbols-outlined">add_box</button>
         </div>
         <p class="amount">${amount + ' st' + ' ' + sum}:-</p>
+        ${/*Add class suffix for styling of stars*/''}
         <div class="productRating productRating-${productRatingSuffix}" aria-label="Rating ${product.rating}">
             <span class="material-symbols-outlined">star</span>
             <span class="material-symbols-outlined">star</span>
@@ -156,8 +186,75 @@ for (let i = 0; i < products.length; i++) {
             <span class="material-symbols-outlined">star</span>
             <span class="material-symbols-outlined">star</span>
         </div>
-
         </article>
     `;
+    }
+
+    const minusButtons = document.querySelectorAll('.minus');
+    const plusButtons = document.querySelectorAll('.plus'); 
+
+    minusButtons.forEach(btn => {
+        btn.addEventListener('click', decreaseAmount);    
+    });
+
+    plusButtons.forEach(btn => {
+        btn.addEventListener('click', increaseAmount);
+    });
+
+    printCartProducts();
+
+};
+
+function printCartProducts() {
+    cartContainer.innerHTML = '';
+    
+    let footerSum = 0;
+    let footerAmount = 0;
+    
+    products.forEach((product, j) => {
+        if (product.amount > 0) {
+            const amount = product.amount;
+            const sum = amount * product.price;
+            footerSum += sum;
+            footerAmount += amount;
+            cartContainer.innerHTML += `
+            <article class="cartRow">
+                <h4>${product.name}</h4>
+                <div>
+                    <button data-id="${j}" class="cartMinus material-symbols-outlined">indeterminate_check_box</button>            
+                    <p class="amount">${amount + ' st'}</p>
+                    <button data-id="${j}" class="cartPlus material-symbols-outlined">add_box</button>
+                </div>
+                <h4 class=productPrice><span>Pris: </span>${product.price}:-</h4>
+                <p class="sum">${sum}:-</p>
+                <button data-id="${j}" class="cartDelete material-symbols-outlined">cancel</button>
+            </article>
+            ` 
+        }
+    });
+    const cartFooterAmount  = document.querySelector('#cartFooterAmount');
+    const cartFooterSum = document.querySelector('#cartFooterSum');
+
+    cartFooterAmount.innerHTML = footerAmount;
+    cartFooterSum.innerHTML = footerSum;
+
+
+    const cartMinusButtons = document.querySelectorAll('.cartMinus');
+    const cartPlusButtons = document.querySelectorAll('.cartPlus'); 
+    const cartDeleteButtons = document.querySelectorAll('.cartDelete')
+
+    cartMinusButtons.forEach(btn => {
+        btn.addEventListener('click', decreaseAmount);    
+    });
+
+    cartPlusButtons.forEach(btn => {
+        btn.addEventListener('click', increaseAmount);
+    });
+
+    cartDeleteButtons.forEach(btn => {
+        btn.addEventListener('click', resetAmount);
+    });
 
 }
+
+printProducts();
